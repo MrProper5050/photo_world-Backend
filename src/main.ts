9e55921fa9b3bad60eba75c124af130a5c734134
MrProper5050 <MrProper5050@yandex.ru>
2020-12-12 05:41:41 -0800

    added> show user images{handlebars}_fixed2

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(cookieParser(process.env.COOKIE_SECRET))
-  app.setBaseViewsDir(join(__dirname,'..','views'))
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.setBaseViewsDir(join(__dirname,'..','views'));
   app.setViewEngine('hbs');
-  await app.listen(3000);
+  await app.listen(3000); 
+
 }
 bootstrap();
